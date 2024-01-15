b7223d96fc3e3e8bde5241218c9b6ff944352928
Untitled-W <2995000455@qq.com>
2024-01-15 23:27:39 +0800

    add generator32 and discriminator32

--- utils/model.py
+++ utils/model.py
@@ -11,35 +11,270 @@ conditioned / unconditioned control, etc.
 '''
 import torch
 
-class Discriminator(torch.nn.Module):
-    def __init__(self, resolution=64):
+class SE(torch.nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(SE, self).__init__()
+        self.avg_pool = torch.nn.AdaptiveAvgPool3d(1)
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(in_channels, in_channels // reduction_ratio),
+            torch.nn.ReLU,
+            torch.nn.Linear(in_channels // reduction_ratio, in_channels),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1, 1)
+        return x * y
+    
+class Conv3DforD(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(Conv3DforD, self).__init__()
+        self.c1 = torch.nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding)
+        self.ac = torch.nn.LeakyReLU(0.2)
+        self.se = SE(out_channels)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.ac(x)
+        x = self.se(x)
+        return x
+    
+
+class Conv3DforG(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(Conv3DforG, self).__init__()
+        self.c1 = torch.nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = torch.nn.BatchNorm3d(out_channels)
+        self.ac = torch.nn.ReLU()
+        self.se = SE(out_channels)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.bn(x)
+        x = self.ac(x)
+        x = self.se(x)
+        return x
+    
+class TransConv3DforG(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(TransConv3DforG, self).__init__()
+        self.c1 = torch.nn.ConvTranspose3d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = torch.nn.BatchNorm3d(out_channels)
+        self.ac = torch.nn.ReLU()
+        self.se = SE(out_channels)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.bn(x)
+        x = self.ac(x)
+        x = self.se(x)
+        return x
+
+
+class Discriminator32(torch.nn.Module):
+    def __init__(self):
         # initialize superior inherited class, necessary hyperparams and modules
         # You may use torch.nn.Conv3d(), torch.nn.sequential(), torch.nn.BatchNorm3d() for blocks
         # You may try different activation functions such as ReLU or LeakyReLU.
         # REMENBER YOU ARE WRITING A DISCRIMINATOR (binary classification) so Sigmoid
         # Dele return in __init__
-        # TODO
-        return
+        super(Discriminator32, self).__init__()
+        # Encode for Voxel
+        # 32^3*1  -> 32^3*32
+        self.encoder1 = Conv3DforD(1, 32, 5, 1, 2)
+        # 32^3*32 -> 16^3*32
+        self.encoder2 = Conv3DforD(32, 32, 4, 2, 1)
+        # 16^3*32 -> 8^3 *64
+        self.encoder3 = Conv3DforD(32, 64, 4, 2, 1)
+        # 8^3 *64 -> 4^3 *128
+        self.encoder4 = Conv3DforD(64, 128, 4, 2, 1)
+        # 4^3 *128-> 2^3 *256
+        self.encoder5 = Conv3DforD(128, 256, 4, 2, 1)
+        # 2^3 *256-> 1024 
+        self.encoderv = torch.nn.Sequential(
+            torch.nn.Flatten(),
+            torch.nn.Linear(1024,1024),
+            torch.nn.LeakyReLU(0.2)
+        )
+        # Encode for label
+        self.encoderl = torch.nn.Sequential(
+            torch.nn.Embedding(11,64),
+            torch.nn.Flatten(),
+            torch.nn.Linear(64,1024),
+            torch.nn.LeakyReLU(0.2)
+        )
+        self.final = torch.nn.Sequential(
+            torch.nn.Linear(1024+1024,512),
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.Linear(512,1)
+        )
+
     
     
-    def forward(self, x):
+    def forward(self, voxel, label):
         # Try to connect all modules to make the model operational!
         # Note that the shape of x may need adjustment
-        # # Do not forget the batch size in x.dim
-        # TODO
+        # Do not forget the batch size in x.dim
+
+        b = voxel.shape[0]
+        voxel.reshape(b,1,32,32,32)
+        
+        # Encode for Voxel
+        # 32^3*1  -> 32^3*32
+        assert voxel.shape == (b,1,32,32,32)
+        v1 = self.encoder1(voxel)
+        # 32^3*32 -> 16^3*32
+        assert v1.shape == (b,32,32,32,32)
+        v2 = self.encoder2(v1)
+        # 16^3*32 -> 8^3 *64
+        assert v.shape == (b,32,16,16,16)
+        v3 = self.encoder3(v2)
+        # 8^3 *64 -> 4^3 *128
+        assert v.shape == (b,64,8,8,8)
+        v4 = self.encoder4(v3)
+        # 4^3 *128-> 2^3 *256
+        assert v.shape == (b,128,4,4,4)
+        v5 = self.encoder5(v4)
+        # 2^3 *256-> 1024 
+        assert v.shape == (b,256,2,2,2)
+        v = self.encoderv(v5)
+
+        # Encode for label:1->1024
+        l = self.encoderl(label)
+
+        # final out
+        assert v.shape == (b,1024)
+        assert l.shape == (b,1024)
+        inp = torch.cat((v,l),dim=1)
+        assert inp.shape == (b,1024*2)
+        out = self.final(out)
+        out.reshape(b)
+        assert out.shape == (b)
+
         return out
         
     
-class Generator(torch.nn.Module):
+class Generator32(torch.nn.Module):
     # TODO
-    def __init__(self, cube_len=64, z_latent_space=64, z_intern_space=64):
+    def __init__(self):
         # similar to Discriminator
         # Despite the blocks introduced above, you may also find torch.nn.ConvTranspose3d()
-        # Dele return in __init__
-        # TODO
-        return
+        
+        super(Generator32, self).__init__()
+
+        # Encode for Voxel
+        # 32^3*1  -> 32^3*32
+        self.encoder1 = Conv3DforG(1, 32, 5, 1, 2)
+        # 32^3*32 -> 16^3*32
+        self.encoder2 = Conv3DforG(32, 32, 4, 2, 1)
+        # 16^3*32 -> 8^3 *64
+        self.encoder3 = Conv3DforG(32, 64, 4, 2, 1)
+        # 8^3 *64 -> 4^3 *128
+        self.encoder4 = Conv3DforG(64, 128, 4, 2, 1)
+        # 4^3 *128-> 2^3 *256
+        self.encoder5 = Conv3DforG(128, 256, 4, 2, 1)
+        # 2^3 *256-> 1024 
+        self.encoderv = torch.nn.Sequential(
+            torch.nn.Flatten(),
+            torch.nn.Linear(1024,1024),
+            torch.nn.ReLU()
+        )
+
+        # Encode for label:1->1024
+        self.encoderl = torch.nn.Sequential(
+            torch.nn.Embedding(11,64),
+            torch.nn.Flatten(),
+            torch.nn.Linear(64,1024),
+            torch.nn.ReLU()
+        )
+
+        # Concat and Transpose
+        self.concat = torch.nn.Linear(1024+1024,1024)
+
+        # Decode
+        # 2^3 *256 -> 4^3 *128 
+        self.decoder1 = TransConv3DforG(256*2, 128, 4, 2, 1)
+        # 4^3 *128 -> 8^3 *64  
+        self.decoder2 = TransConv3DforG(128*2, 64, 4, 2, 1)
+        # 8^3 *64  -> 16^3*32  
+        self.decoder3 = TransConv3DforG(64*2, 32, 4, 2, 1)
+        # 16^3*32  -> 32^3*32  
+        self.decoder4 = TransConv3DforG(32*2, 32, 4, 2, 1)
+        # 32^3*32  -> 32^3*1   
+        self.decoder5 = torch.nn.Sequential(
+            torch.nn.ConvTranspose3d(32*2, 1, 5, 1, 2),
+            torch.nn.Tanh(),
+            SE(1)
+        )
+
     
-    def forward(self, x):
+    def forward(self, voxel, label):
         # you may also find torch.view() useful
         # we strongly suggest you to write this method seperately to forward_encode(self, x) and forward_decode(self, x)   
+        b = voxel.shape[0]
+        voxel.reshape(b,1,32,32,32)
+        
+        # Encode for Voxel
+        # 32^3*1  -> 32^3*32
+        assert voxel.shape == (b,1,32,32,32)
+        v1 = self.encoder1(voxel)
+        # 32^3*32 -> 16^3*32
+        assert v1.shape == (b,32,32,32,32)
+        v2 = self.encoder2(v1)
+        # 16^3*32 -> 8^3 *64
+        assert v.shape == (b,32,16,16,16)
+        v3 = self.encoder3(v2)
+        # 8^3 *64 -> 4^3 *128
+        assert v.shape == (b,64,8,8,8)
+        v4 = self.encoder4(v3)
+        # 4^3 *128-> 2^3 *256
+        assert v.shape == (b,128,4,4,4)
+        v5 = self.encoder5(v4)
+        # 2^3 *256-> 1024 
+        assert v.shape == (b,256,2,2,2)
+        v = self.encoderv(v5)
+
+        # Encode for label:1->1024
+        l = self.encoderl(label)
+
+        # Concat and Transpose
+        assert v.shape == (b,1024)
+        assert l.shape == (b,1024)
+        inp = torch.cat((v,l),dim=1)
+        assert inp.shape == (b,1024+1024)
+        inp = self.concat(inp)
+        inp = inp.reshape(b,256,2,2,2)
+        assert inp.shape == (b,256,2,2,2)
+
+        # Decode
+        # 2^3 *256 -> 4^3 *128 
+        d5 = torch.concat(inp,v5)
+        assert d5.shape == (b,256*2,2,2,2)
+        d5 = self.decoder1(d5)
+        # 4^3 *128 -> 8^3 *64  
+        assert d5.shape == (b,128,4,4,4)
+        d4 = torch.concat(d5,v4)
+        assert d4.shape == (b,128*2,4,4,4)
+        d4 = self.decoder2(d4)
+        # 8^3 *64  -> 16^3*32  
+        assert d4.shape == (b,64,8,8,8)
+        d3 = torch.concat(d4,v3)
+        assert d3.shape == (b,64*2,8,8,8)
+        d3 = self.decoder3(d3)
+        # 16^3*32  -> 32^3*32  
+        assert d3.shape == (b,32,16,16,16)
+        d2 = torch.concat(d3,v2)
+        assert d2.shape == (b,32*2,16,16,16)
+        d2 = self.decoder4(d2)
+        # 32^3*32  -> 32^3*1   
+        assert d2.shape == (b,32,32,32,32)
+        d1 = torch.concat(d2,v1)
+        assert d1.shape == (b,32*2,32,32,32)
+        d1 = self.decoder5(d1)
+
+        out = d1.reshape(b,32,32,32)
+        assert out.shape == (b,32,32,32)
+
         return out
